c2bcd30901e522224a5d0a1fa3ebbfc8050ece93
delphinus <me@delphinus.dev>
2022-12-15 20:57:11 +0900

    Run also in VimResized

--- lua/qfheight/main.lua
+++ lua/qfheight/main.lua
@@ -20,16 +20,43 @@ end
 ---@return nil
 function Qfheight:setup(opts)
   self.opts = vim.tbl_extend("force", self.default_options, opts)
+  vim.validate {
+    max = {
+      self.opts.max,
+      function(v)
+        return type(v) == "number" and self.opts.max >= self.opts.min
+      end,
+      "number and max >= min",
+    },
+    min = {
+      self.opts.min,
+      function(v)
+        return type(v) == "number" and self.opts.max >= self.opts.min
+      end,
+      "number and max >= min",
+    },
+  }
   self:enable()
 end
 
 ---@return nil
 function Qfheight:enable()
   self:disable()
-  vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
-    callback = function()
-      if vim.opt.buftype:get() == "quickfix" then
-        self:set()
+  vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "VimResized" }, {
+    group = self.augroup,
+    ---@param args { event: string }
+    callback = function(args)
+      if args.event == "VimResized" then
+        ---@type integer[]
+        local quickfixes = vim.tbl_filter(function(winid)
+          local bufnr = vim.api.nvim_win_get_buf(winid)
+          return vim.api.nvim_buf_get_option(bufnr, "buftype") == "quickfix"
+        end, vim.api.nvim_list_wins())
+        for _, winid in ipairs(quickfixes) do
+          self:set(winid)
+        end
+      elseif vim.opt.buftype:get() == "quickfix" then
+        self:set(0)
       end
     end,
   })
@@ -40,13 +67,16 @@ function Qfheight:disable()
   vim.api.nvim_clear_autocmds { group = self.augroup }
 end
 
-function Qfheight:set()
+---@param winid integer
+---@return nil
+function Qfheight:set(winid)
   local i = 0
   local count = 0
-  local last = vim.fn.line "$" - 1
-  local winwidth = vim.fn.winwidth(0)
+  local last = vim.fn.line("$", winid) - 1
+  local winwidth = vim.fn.winwidth(winid)
+  local bufnr = vim.api.nvim_win_get_buf(winid)
   while i <= last do
-    local line = vim.api.nvim_buf_get_lines(0, i, i + 1, false)
+    local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)
     if #line == 0 then
       break
     end
@@ -58,7 +88,7 @@ function Qfheight:set()
     i = i + 1
   end
   local height = math.max(math.min(count, self.opts.max), self.opts.min)
-  vim.api.nvim_win_set_height(0, height)
+  vim.api.nvim_win_set_height(winid, height)
 end
 
 ---@param msg string
